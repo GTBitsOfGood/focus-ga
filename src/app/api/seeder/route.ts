50e7264290ee2621ecec328ce95bed8b41ab5883
Lauren Ji <lyangji1011@gmail.com>
2024-09-21 17:21:35 -0400

    create posts

--- src/app/api/seeder/route.ts
+++ src/app/api/seeder/route.ts
@@ -10,10 +10,11 @@ import ReportModel from '@/server/db/models/ReportModel';
 import UserModel from '@/server/db/models/UserModel';
 import { createDisability } from '@/server/db/actions/DisabilityActions';
 import { createUser } from '@/server/db/actions/UserActions';
+import { createPost } from '@/server/db/actions/PostActions';
 
 const disabilities = ["Cerebral Palsy", "Autism Spectrum Disorder", "Down Syndrome", "Spina Bifida", "Muscular Dystrophy", "Rett Syndrome", "Fragile X Syndrome", "Epilepsy", "ADHD", "Spinal Muscular Atrophy"];
 
-export default async function POST(request: Request) {
+export async function POST(request: Request) {
   await dbConnect();
 
   // clear the database
@@ -30,19 +31,30 @@ export default async function POST(request: Request) {
   const disabilityIds = []
   for (const disability in disabilities) {
     const id = (await createDisability({ name: disability }))._id;
-    disabilityIds.push(id.toString());
+    disabilityIds.push(id);
   }
+
 
   // create users
   const users = [];
   const userIds = [];
   for (let i = 0; i < 10; i++) {
+    const randomDisabilityCount = Math.floor(Math.random() * 10) + 1;
+    const availableDisabilities = [...disabilityIds];
+    const selectedDisabilities: string[] = [];
+
+    for (let i = 0; i < randomDisabilityCount; i++) {
+      const randomIndex = Math.floor(Math.random() * availableDisabilities.length);
+      selectedDisabilities.push(disabilityIds[randomIndex]);
+      availableDisabilities.splice(randomIndex, 1); // Remove selected number to avoid duplicates
+    }
+
     const userInfo = {
       username: faker.internet.userName(),
       lastName: faker.person.lastName(),
       email: faker.internet.email(),
       childAge: Math.floor(Math.random() * (20)) + 1, // generates random age between 1 and 20
-      childDisabilities: [],
+      childDisabilities: selectedDisabilities,
       address: {
         street: faker.location.streetAddress(),
         city: faker.location.city(),
@@ -50,10 +62,42 @@ export default async function POST(request: Request) {
         zipCode: faker.location.zipCode(),
       }
     }
-    // TODO: choose a random number of disabilities for each user
+
     users.push(userInfo);
     const id = (await createUser(userInfo))._id;
-    userIds.push(id.toString());
+    userIds.push(id);
+  }
+
+
+  // create posts
+  const posts = [];
+  const postIds = [];
+  for (const userId in userIds) {
+    const numberOfPosts = Math.floor(Math.random() * (16)) + 10;
+
+    for (let i = 0; i < numberOfPosts; i++) {
+      const randomDisabilityCount = Math.floor(Math.random() * 10) + 1;
+      const availableDisabilities = [...disabilityIds];
+      const selectedDisabilities: string[] = [];
+
+      for (let j = 0; j < randomDisabilityCount; j++) {
+        const randomIndex = Math.floor(Math.random() * availableDisabilities.length);
+        selectedDisabilities.push(disabilityIds[randomIndex]);
+        availableDisabilities.splice(randomIndex, 1); // Remove selected number to avoid duplicates
+      }
+
+      const postInfo = {
+        author: userId, 
+        date: Math.random() < 0.5 ? faker.date.past({ years: 4 }) : undefined,
+        title: faker.lorem.words({ min: 3, max: 14 }),
+        content: faker.lorem.paragraph({ min: 3, max: 10 }),
+        tags: selectedDisabilities,
+      }
+
+      posts.push(postInfo);
+      const id = ((await createPost(postInfo))._id);
+      postIds.push(id);
+    }
   }
 
   return Response.json("success", { status: 200 })
