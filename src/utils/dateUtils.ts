b0446bda29d9af11f8c3b3ff91fcd5cbda3a7ba3
ngong8 <nathangong@gatech.edu>
2024-09-29 18:09:49 -0400

    Refactor dates

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -1,19 +1,30 @@
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const WEEK = DAY * 7;
+
 export function getDateDifferenceString(date1: Date, date2: Date) {
   const msDiff = Math.abs(Math.floor(date1.getTime() - date2.getTime()));
+  
+  if (msDiff >= WEEK) {
+    return date2.toLocaleDateString('en-US', { month: 'numeric', day: 'numeric', year: 'numeric' });
+  }
+
   let diff: number;
   let diffUnit: string;
 
-  if (msDiff >= 1000 * 60 * 60 * 24) {
-    diff = Math.floor(msDiff / (1000 * 60 * 60 * 24));
+  if (msDiff >= DAY) {
+    diff = Math.floor(msDiff / DAY);
     diffUnit = 'day';
-  } else if (msDiff >= 1000 * 60 * 60) {
-    diff = Math.floor(msDiff / (1000 * 60 * 60));
+  } else if (msDiff >= HOUR) {
+    diff = Math.floor(msDiff / HOUR);
     diffUnit = 'hour';
-  } else if (msDiff >= 1000 * 60) {
-    diff = Math.floor(msDiff / (1000 * 60));
+  } else if (msDiff >= MINUTE) {
+    diff = Math.floor(msDiff / MINUTE);
     diffUnit = 'minute';
   } else {
-    diff = Math.floor(msDiff / 1000);
+    diff = Math.floor(msDiff / SECOND);
     diffUnit = 'second';
   }
 
