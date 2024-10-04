65d39213d7515954f502194f02fe4694b3d5c246
ngong8 <nathangong@gatech.edu>
2024-10-04 00:12:33 -0400

    Force dynamic rendering of community feed

--- src/app/(community)/page.tsx
+++ src/app/(community)/page.tsx
@@ -3,6 +3,8 @@
 import { getPopulatedPosts } from "@/server/db/actions/PostActions";
 import PostComponent from "@/components/PostComponent";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const posts = await getPopulatedPosts();
 
