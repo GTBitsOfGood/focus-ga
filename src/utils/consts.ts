07ad7f504fe14878b23c578a21817c30fd96b576
ngong8 <nathangong@gatech.edu>
2024-10-07 22:30:44 -0400

    Fix styling issue

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,3 +1,10 @@
+import { Lato } from "next/font/google";
+
 export const MAX_POST_TITLE_LEN = 100;
 export const MAX_POST_CONTENT_LEN = 5000;
 export const MAX_POST_DISABILITY_TAGS = 5;
+
+export const FOCUS_FONT = Lato({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700', '900'],
+});
