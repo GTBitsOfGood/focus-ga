aefffacb7233bdf72618c3b104c6a6060623bf5f
Lauren Ji <lyangji1011@gmail.com>
2024-10-13 14:40:34 -0400

    disability tags

--- src/components/ProfilePage/ProfileContainer.tsx
+++ src/components/ProfilePage/ProfileContainer.tsx
@@ -3,7 +3,9 @@ import { ChevronLeft } from "lucide-react";
 import { useRouter } from 'next/navigation';
 import { Separator } from "@/components/ui/separator";
 import { User } from "@/utils/types/user";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { getDisability } from "@/server/db/actions/DisabilityActions";
+import Tag from "../Tag";
 
 type ProfileContainerProps = {
   user: User;
@@ -11,11 +13,26 @@ type ProfileContainerProps = {
 
 export default function ProfileContainer({user}: ProfileContainerProps) {
   const router = useRouter();
+  const [disabilities, setDisabilities] = useState<string[]>([]);
   console.log(user);
 
   useEffect(() => {
-    // TODO: get disability objects for user's child disabilities
-  }, []);
+    const fetchDisabilities = async () => {
+      try {
+        const disabilitiesData = await Promise.all(
+          user.childDisabilities.map(async (disability) => {
+            const disabilityData = await getDisability(disability.toString());
+            return disabilityData.name;
+          })
+        );
+        setDisabilities(disabilitiesData);
+      } catch (error) {
+        console.error("Error fetching disabilities: ", error);
+      }
+    };
+  
+    fetchDisabilities();
+  }, [user.childDisabilities]);
 
   const handleBack = () => {
     router.push("/");
@@ -27,7 +44,7 @@ export default function ProfileContainer({user}: ProfileContainerProps) {
         <ChevronLeft color="#636363" />
         <p className="text-focus-gray">Back</p>
       </div>
-      <div className="mx-20">
+      <div className="mx-20 mt-8">
         <div className="flex flex-row mb-6 items-center">
           <div className="flex flex-col">
             <p className="text-2xl font-bold">{user.lastName} Household</p>
@@ -35,14 +52,20 @@ export default function ProfileContainer({user}: ProfileContainerProps) {
           </div>
         </div>
         <div>
-          <p className="text-lg">
+          <p className="text-lg mb-4">
             <span className="font-semibold">Location: </span>
             <span className="text-focus-gray">{user.city}, GA</span>
           </p>
-          <p className="text-lg">
-            <span className="font-semibold">Disabilities: </span>
-            <span className="text-focus-gray">info</span>
-          </p>
+          <div className="flex flex-row mb-4">
+            <p className="text-lg font-semibold mr-3">Disabilities: </p>
+            <div className="flex flex-row space-x-3">
+              {
+                disabilities.map((disability, index) => {
+                  return <Tag text={disability} key={index} />
+                })
+              }
+            </div>
+          </div>
           <p className="font-semibold text-lg">Bio</p>
           <p className="text-lg text-focus-gray">{user.bio}</p>
         </div>
