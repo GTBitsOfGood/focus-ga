3b4a1b43718e2b58325350dd858b50134fae8bd9
Yatharth Bhargava <yatharthbhargava@gmail.com>
2024-10-18 19:25:31 -0400

    nit

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,7 @@ import { SessionData, sessionOptions } from "@/lib/session";
 import { cookies } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-  console.log(request)
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
-  console.log(session)
   if (!session.isLoggedIn) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
