3036ba2b5df4c819614ce7f6a2078f310453edb4
ngong8 <nathangong@gatech.edu>
2024-11-04 21:01:58 -0500

    Cache user in auth actions

--- src/server/db/actions/AuthActions.ts
+++ src/server/db/actions/AuthActions.ts
@@ -7,6 +7,8 @@ import { getUser, signOut } from './UserActions';
 import { User } from '@/utils/types/user';
 import { redirect } from 'next/navigation';
 
+let user: User | null;
+
 /**
  * Retrieves the authenticated user from the session.
  * If the user is not logged in, this function returns null.
@@ -16,11 +18,16 @@ export async function getAuthenticatedUser(): Promise<User | null> {
   const session = await getIronSession<SessionData>(cookies(), sessionOptions);
 
   if (!session.isLoggedIn) {
+    user = null;
     return null;
   }
 
+  if (user) {
+    return user;
+  }
+
   try {
-    const user = await getUser(session.userId);
+    user = await getUser(session.userId);
     return user;
   } catch (e) {
     await signOut();
