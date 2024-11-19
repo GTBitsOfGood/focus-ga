2de04aabf0d2675e324be50503e0b56907f05d90
ngong8 <nathangong@gatech.edu>
2024-11-18 23:13:02 -0500

    revert cleanup

--- src/components/ProfilePage/ProfileContainer.tsx
+++ src/components/ProfilePage/ProfileContainer.tsx
@@ -5,7 +5,7 @@ import { Separator } from "@/components/ui/separator";
 import { PopulatedUser } from "@/utils/types/user";
 import { useEffect, useState } from "react";
 import Tag from "../Tag";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { getPopulatedSavedPosts, getPopulatedUserPosts } from "@/server/db/actions/PostActions";
 import { PopulatedPost } from "@/utils/types/post";
 import PostComponent from "../PostComponent";
@@ -17,84 +17,10 @@ import { useUser } from "@/contexts/UserContext";
 import BackButton from "../BackButton";
 import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle } from "../ui/alert-dialog";
 
+
 type ProfileContainerProps = {
   user: PopulatedUser;
-};
-
-const ProfileHeader = ({ user, currUser, openModal, setShowBanDialog, isBanned, banLoading }: any) => (
-  <div className="flex flex-row mb-6 items-start justify-between">
-    <div className="flex flex-row space-x-6">
-      <div className={`flex items-center justify-center w-[108px] h-[108px] rounded-full bg-${user.profileColor} relative`}>
-        <span className="text-6xl select-none font-medium text-black">{user.lastName.charAt(0).toUpperCase()}</span>
-        {user._id === currUser._id && <ColorPicker handleColorPick={openModal} />}
-      </div>
-      <div className="flex flex-col justify-center">
-        <p className="text-2xl font-bold">{user.lastName} Family</p>
-        <p className="text-lg font-normal">{user.email}</p>
-      </div>
-    </div>
-    {user._id === currUser._id ? (
-      <button onClick={openModal} className="bg-light-gray hover:bg-zinc-300 transition text-theme-gray text-lg font-bold px-4 py-2 rounded-lg">
-        <div className="flex flex-row items-center space-x-2.5">
-          <Pencil color="#636363" className="w-6 h-6" />
-          <p>Edit</p>
-        </div>
-      </button>
-    ) : currUser.isAdmin && (
-      <button onClick={() => setShowBanDialog(true)} className={`'bg-light-gray ${isBanned ? 'hover:bg-theme-gray hover:text-gray-200' : 'hover:bg-zinc-300'} transition text-theme-gray text-lg font-bold px-4 py-2 rounded-lg`}>
-        <div className="flex flex-row items-center space-x-2.5">
-          <Ban className="w-6 h-6" />
-          <p>{user.isBanned ? 'Unban User' : 'Ban User'}</p>
-        </div>
-      </button>
-    )}
-  </div>
-);
-
-const ProfileDetails = ({ user }: any) => (
-  <div>
-    <p className="text-lg mb-4">
-      <span className="font-semibold">Location: </span>
-      <span className="text-theme-gray ml-1">{user.city}, GA</span>
-    </p>
-    <div className="flex flex-row mb-4">
-      <p className="text-lg font-semibold mr-3">Disabilities: </p>
-      <div className={`flex flex-row flex-wrap gap-3 ${user.childDisabilities.length > 0 ? 'py-1' : '-my-1'}`}>
-        {user.childDisabilities.map((disability: any, index: number) => (
-          <Tag text={disability.name} key={index} />
-        ))}
-      </div>
-      {user.childDisabilities.length === 0 && <p className="text-lg text-theme-gray -ml-1">N/A</p>}
-    </div>
-    <div className="flex flex-row gap-2">
-      {user.bio && <p className="font-semibold text-lg">Bio:</p>}
-      <p className="text-lg text-theme-gray break-words overflow-hidden">{user.bio}</p>
-    </div>
-  </div>
-);
-
-const ProfileTabs = ({ user, currUser, userPosts, savedPosts }: any) => (
-  <Tabs defaultValue="my-posts">
-    <TabsList className="mb-4">
-      <TabsTrigger value="my-posts">{user._id === currUser._id ? 'My Posts' : 'Posts'}</TabsTrigger>
-      {user._id === currUser._id && <TabsTrigger value="saved-posts">Saved Posts</TabsTrigger>}
-    </TabsList>
-    <TabsContent value="my-posts">
-      <div className="space-y-6">
-        {userPosts.map((post: any) => (
-          <PostComponent key={post._id} post={post} clickable={true} />
-        ))}
-      </div>
-    </TabsContent>
-    <TabsContent value="saved-posts">
-      <div className="space-y-6">
-        {savedPosts.map((post: any) => (
-          <PostComponent key={post._id} post={post} clickable={true} />
-        ))}
-      </div>
-    </TabsContent>
-  </Tabs>
-);
+}
 
 export default function ProfileContainer({ user }: ProfileContainerProps) {
   const [userPosts, setUserPosts] = useState<PopulatedPost[]>([]);
@@ -103,7 +29,7 @@ export default function ProfileContainer({ user }: ProfileContainerProps) {
   const [showBanDialog, setShowBanDialog] = useState<boolean>(false);
   const [banLoading, setBanLoading] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { user: currUser, setUser } = useUser();
+  const { user: currUser, setUser } = useUser(); 
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -117,34 +43,38 @@ export default function ProfileContainer({ user }: ProfileContainerProps) {
     };
 
     if (user) {
-      fetchUserPosts();
+      fetchUserPosts(); 
       fetchSavedPosts();
     }
   }, [user]);
 
   const handleColorPick = async (color: ProfileColors) => {
-    if (!currUser) return;
+    if (!currUser) {
+      return;
+    }
     try {
       const updatedUser = await editUser(currUser._id, { profileColor: color });
       setUser({ ...currUser, profileColor: color });
     } catch (error) {
       console.error("Failed to update profile color: ", error);
     }
-  };
+  }
 
   const handleBanClick = async () => {
     if (banLoading) return;
     setBanLoading(true);
     try {
       await editUser(user._id, { isBanned: !isBanned });
-      setIsBanned((isBanned) => !isBanned);
+      setIsBanned(isBanned => !isBanned);
       setShowBanDialog(false);
     } finally {
       setBanLoading(false);
     }
-  };
+  }
 
-  if (!currUser) return null;
+  if (!currUser) {
+    return;
+  }
 
   return (
     <div>
@@ -153,14 +83,36 @@ export default function ProfileContainer({ user }: ProfileContainerProps) {
       </div>
       <div className="mx-16 my-4">
         <div className="mx-14 mt-8">
-          <ProfileHeader
-            user={user}
-            currUser={currUser}
-            openModal={openModal}
-            setShowBanDialog={setShowBanDialog}
-            isBanned={isBanned}
-            banLoading={banLoading}
-          />
+          <div className="flex flex-row mb-6 items-start justify-between">
+            <div className="flex flex-row space-x-6">
+              <div className={`flex items-center justify-center w-[108px] h-[108px] rounded-full bg-${user.profileColor} relative`}> 
+                <span className="text-6xl select-none font-medium text-black">{user.lastName.charAt(0).toUpperCase()}</span>
+                { user._id === currUser._id && <ColorPicker handleColorPick = {handleColorPick} /> }
+              </div>
+              <div className="flex flex-col justify-center">
+                <p className="text-2xl font-bold">{user.lastName} Family</p>
+                <p className="text-lg font-normal">{user.email}</p>
+              </div>
+            </div>
+            {
+              user._id === currUser._id
+              ? (
+                <button onClick={() => setIsModalOpen(true)} className="bg-light-gray hover:bg-zinc-300 transition text-theme-gray text-lg font-bold px-4 py-2 rounded-lg">
+                  <div className="flex flex-row items-center space-x-2.5">
+                    <Pencil color="#636363" className="w-6 h-6" />
+                    <p>Edit</p>
+                  </div>
+                </button>
+              ) : currUser.isAdmin && (
+                <button onClick={() => setShowBanDialog(true)} className={`'bg-light-gray ${isBanned ? 'hover:bg-theme-gray hover:text-gray-200' : 'hover:bg-zinc-300'} transition text-theme-gray text-lg font-bold px-4 py-2 rounded-lg`}>
+                  <div className="flex flex-row items-center space-x-2.5">
+                    <Ban className="w-6 h-6" />
+                    <p>{user.isBanned ? 'Unban User' : 'Ban User'}</p>
+                  </div>
+                </button>
+              )
+            }
+          </div>
           <EditProfileModal
             id={user._id}
             originalLocation={user.city}
@@ -175,9 +127,11 @@ export default function ProfileContainer({ user }: ProfileContainerProps) {
               <AlertDialogHeader>
                 <AlertDialogTitle>{isBanned ? 'Unban User' : 'Ban User'}</AlertDialogTitle>
                 <AlertDialogDescription>
-                  {isBanned
-                    ? 'Are you sure you want to unban this user?'
-                    : 'Are you sure you want to ban this user? They would not be able to view any future posts.'}
+                  {
+                    isBanned
+                      ? 'Are you sure you want to unban this user?'
+                      : 'Are you sure you want to ban this user? They would not be able to view any future posts.'
+                  }
                 </AlertDialogDescription>
               </AlertDialogHeader>
               <AlertDialogFooter>
@@ -187,16 +141,63 @@ export default function ProfileContainer({ user }: ProfileContainerProps) {
                   onClick={handleBanClick}
                   className="bg-theme-blue hover:bg-theme-blue hover:opacity-90 transition"
                 >
-                  {isBanned ? (banLoading ? 'Unbanning...' : 'Unban') : (banLoading ? 'Banning...' : 'Ban')}
+                  {
+                    isBanned
+                      ? (banLoading ? 'Unbanning...' : 'Unban')
+                      : (banLoading ? 'Banning...' : 'Ban')
+                  }
                 </AlertDialogAction>
               </AlertDialogFooter>
             </AlertDialogContent>
           </AlertDialog>
-          <ProfileDetails user={user} />
+          <div>
+            <p className="text-lg mb-4">
+              <span className="font-semibold">Location: </span>
+              <span className="text-theme-gray ml-1">{user.city}, GA</span>
+            </p>
+            <div className="flex flex-row mb-4">
+              <p className="text-lg font-semibold mr-3">Disabilities: </p>
+              <div className={`flex flex-row flex-wrap gap-3 ${user.childDisabilities.length > 0 ? 'py-1' : '-my-1'}`}>
+                {
+                  user.childDisabilities.map((disability, index) => {
+                    return <Tag text={disability.name} key={index} />
+                  })
+                }
+              </div>
+              {user.childDisabilities.length == 0 ? <p className="text-lg text-theme-gray -ml-1">N/A</p> : <></>}
+            </div>
+            <div className="flex flex-row gap-2">
+              {user.bio ? <p className="font-semibold text-lg">Bio:</p> : <></>}
+              <p className="text-lg text-theme-gray break-words overflow-hidden">{user.bio}</p>
+            </div>
+          </div>
           <Separator className="bg-theme-gray my-6" />
-          <ProfileTabs user={user} currUser={currUser} userPosts={userPosts} savedPosts={savedPosts} />
+          <Tabs defaultValue="my-posts">
+            <TabsList className="mb-4">
+              <TabsTrigger value="my-posts">{user._id === currUser._id ? 'My Posts' : 'Posts'}</TabsTrigger>
+              {user._id === currUser._id && <TabsTrigger value="saved-posts">Saved Posts</TabsTrigger>}
+            </TabsList>
+            <TabsContent value="my-posts">
+              <div className="space-y-6">
+                {
+                  userPosts.map((post) => {
+                    return <PostComponent key={post._id} post={post} clickable={true} />
+                  })
+                }
+              </div>
+            </TabsContent>
+            <TabsContent value="saved-posts">
+              <div className="space-y-6">
+                {
+                  savedPosts.map((post) => {
+                    return <PostComponent key={post._id} post={post} clickable={true} />
+                  })
+                }
+              </div>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
     </div>
-  );
+  )
 }
