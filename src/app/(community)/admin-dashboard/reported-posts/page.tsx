d06c6ea15dd7448a8ed04e1c0250fd49287f91d4
Charlie Lin <lincharlie00@gmail.com>
2025-02-01 20:10:59 -0500

    added reported posts are sorted from most recent report to least recent report

--- src/app/(community)/admin-dashboard/reported-posts/page.tsx
+++ src/app/(community)/admin-dashboard/reported-posts/page.tsx
@@ -18,17 +18,18 @@ export default function ReportedPosts() {
   const fetchUnresolvedPosts = async () => {
     if (!user) return;
     const initReports = await getReports();
-    const unresolvedReportedPostsArr : Types.ObjectId[] = [];
+    const unresolvedReportedPostsArr : {postId : Types.ObjectId, date : Date}[] = [];
     initReports.forEach((report) => {
-      console.log(report)
       const reportedContentId = report.reportedContent;
-      if (!report.isResolved && !unresolvedReportedPostsArr.includes(reportedContentId)) {
-        unresolvedReportedPostsArr.push(reportedContentId);
+      if (!report.isResolved && !unresolvedReportedPostsArr.some(item => item.postId.toString() === reportedContentId.toString())) {
+        unresolvedReportedPostsArr.push({postId : reportedContentId, date: report.date});
       }
     });
+
+    unresolvedReportedPostsArr.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
     let postsArr : PopulatedPost[] = await Promise.all(
-      unresolvedReportedPostsArr.map(async (postId) => {
-        const idAsString = postId.toString();
+      unresolvedReportedPostsArr.map(async (postInfo) => {
+        const idAsString = postInfo.postId.toString();
         return await getPopulatedPost(idAsString, user._id);
       })
     );
@@ -39,9 +40,8 @@ export default function ReportedPosts() {
     <div className="mt-9 max-w-[78%] md:ml-10">
       <h1 className="text-2xl font-bold mb-[33px]">Reported Posts</h1>
       {posts.map((post, index) => {
-                return <PostComponent key={post._id} post={post} clickable={true} />;
-              })
-        }
+        return <PostComponent key={post._id} post={post} clickable={true} />;
+      })}
     </div>
   );
 }
