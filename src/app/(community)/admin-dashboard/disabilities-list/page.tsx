5c5dd09a39408d200749f10457bf675fd6ce2909
Lauren Ji <lyangji1011@gmail.com>
2025-02-03 09:53:18 -0500

    Edit confirmation toast text

--- src/app/(community)/admin-dashboard/disabilities-list/page.tsx
+++ src/app/(community)/admin-dashboard/disabilities-list/page.tsx
@@ -11,7 +11,6 @@ import Tag from "@/components/Tag";
 import { Search } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 
-
 export default function DisabilitiesList() {
   const [disabilities, setDisabilities] = useState<Disability[]>([]);
   const [disabilityName, setDisabilityName] = useState("");
@@ -36,16 +35,18 @@ export default function DisabilitiesList() {
     try {
       const newDisability = await createDisability({ name: disabilityName });
       setDisabilities((prev) =>
-        [...prev, newDisability].sort((a, b) => a.name.localeCompare(b.name))
+        [...prev, newDisability].sort((a, b) => a.name.localeCompare(b.name)),
       );
       setDisabilityName("");
       toast({
-        title: "Disability Added",
+        title: "Disability added",
+        description: `${disabilityName} has been added as a disability.`,
       });
     } catch (error) {
       toast({
         title: "Failed to add disability",
-        description: "An error occurred while adding the disability. Please try again.",
+        description:
+          "An error occurred while adding the disability. Please try again.",
       });
     }
   };
@@ -55,54 +56,56 @@ export default function DisabilitiesList() {
       await deleteDisability(id);
       setDisabilities((prev) => prev.filter((d) => d._id !== id));
       toast({
-        title: "Disability Deleted",
+        title: "Disability deleted",
+        description: `${disabilityName} has been deleted as a disability.`,
       });
     } catch (error) {
       toast({
         title: "Failed to delete disability",
-        description: "An error occurred while deleting the disability. Please try again.",
+        description:
+          "An error occurred while deleting the disability. Please try again.",
       });
     }
   };
 
   const filteredDisabilities = disabilities.filter((disability) =>
-    disability.name.toLowerCase().includes(searchQuery.toLowerCase())
+    disability.name.toLowerCase().includes(searchQuery.toLowerCase()),
   );
 
   const groupedDisabilities = filteredDisabilities.reduce(
     (acc: Record<string, Disability[]>, disability) => {
       const firstLetter = disability.name[0].toUpperCase();
-      if (!acc[firstLetter]){
+      if (!acc[firstLetter]) {
         acc[firstLetter] = [];
       }
       acc[firstLetter].push(disability);
       return acc;
     },
-    {}
+    {},
   );
 
   return (
-    <div className="container mx-auto ml-10 p-6 max-w-4xl mt-5">
-      <h1 className="text-2xl font-bold mb-7">Disabilities List</h1>
-      {error && <div className="text-red-500 mb-4">{error}</div>}
-      <h2 className="text-xl mb-3">Add New Disability</h2>
-      <div className="flex items-center gap-3 mb-6">
+    <div className="container mx-auto ml-10 mt-5 max-w-4xl p-6">
+      <h1 className="mb-7 text-2xl font-bold">Disabilities List</h1>
+      {error && <div className="mb-4 text-red-500">{error}</div>}
+      <h2 className="mb-3 text-xl">Add New Disability</h2>
+      <div className="mb-6 flex items-center gap-3">
         <input
           type="text"
           value={disabilityName}
           onChange={(e) => setDisabilityName(e.target.value)}
           placeholder="Enter disability"
-          className="border p-3 rounded-md w-full py-1.5 text-sm h-10"
+          className="h-10 w-full rounded-md border p-3 py-1.5 text-sm"
         />
         <button
-          className="bg-theme-blue text-white px-6 py-1.5 rounded-md transition hover:opacity-90 h-10"
+          className="h-10 rounded-md bg-theme-blue px-6 py-1.5 text-white transition hover:opacity-90"
           onClick={handleAddDisability}
         >
           Add
         </button>
       </div>
       <div className="mb-6">
-        <div className="flex justify-between items-center mb-6">
+        <div className="mb-6 flex items-center justify-between">
           <h2 className="text-xl">Current Disability List</h2>
           <div className="flex items-center gap-1.5">
             <Search className="text-theme-gray" size={20} />
@@ -111,14 +114,14 @@ export default function DisabilitiesList() {
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
               placeholder="Search..."
-              className="border-b p-1 pl-0 w-60 focus:outline-none focus:border-theme-gray"
+              className="w-60 border-b p-1 pl-0 focus:border-theme-gray focus:outline-none"
             />
           </div>
         </div>
 
         {Object.keys(groupedDisabilities).map((letter) => (
           <div key={letter} className="mb-4">
-            <div className="text-lg mb-2">{letter}</div>
+            <div className="mb-2 text-lg">{letter}</div>
             <div className="flex flex-wrap gap-3">
               {groupedDisabilities[letter].map((disability) => (
                 <Tag
