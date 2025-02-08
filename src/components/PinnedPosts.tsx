b2621d378a417835034aa2d508999cd902ad3520
DanielChee <Danielpchee@gmail.com>
2025-02-08 01:30:13 -0500

    allow liking and saving outside of post.

--- src/components/PinnedPosts.tsx
+++ src/components/PinnedPosts.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import { PopulatedPost } from '@/utils/types/post';
 import PostComponent from './PostComponent';
 import { Pin, ChevronDown, ChevronUp } from 'lucide-react';
@@ -15,7 +16,7 @@ const PinnedPosts: React.FC<PinnedPostsProps> = ({ posts }) => {
   };
 
   return (
-    <div className="relative pinned-posts border-2 border-light-gray rounded-lg p-4">
+    <div className="relative pinned-posts border-2 border-light-gray rounded-lg p-4 pb-1">
       <button onClick={toggleOpen} className="flex justify-between w-full">
         <div className='flex justify-between space-x-2'>
           <Pin/>
@@ -28,16 +29,19 @@ const PinnedPosts: React.FC<PinnedPostsProps> = ({ posts }) => {
           {isOpen ? <ChevronUp /> : <ChevronDown />}
         </div>
       </button>
-      {isOpen && (
-        <div className="mt-2">
-          {posts.map((post) => (
-            <PostComponent key={post._id} post={post} clickable={true} />
-          ))}
-        </div>
-      )}
-
-      {/* Horizontal Bar */}
-      <div className="absolute bottom-3 left-0 right-0 h-1 bg-white" />
+      <motion.div
+        initial={{ height: 0, opacity: 0 }}
+        animate={{ height: isOpen ? 'auto' : 0, opacity: isOpen ? 1 : 0 }}
+        exit={{ height: 0, opacity: 0 }}
+        transition={{ duration: 0.3, ease: 'easeInOut' }}
+        className="overflow-hidden mt-2"
+      >
+        {posts.map((post) => (
+          <PostComponent key={post._id} post={post} clickable={true} />
+        ))}
+      </motion.div>
+    
+      {isOpen && <div className="absolute bottom-3 left-0 right-0 h-1 bg-white" />}
     </div>
   );
 };
