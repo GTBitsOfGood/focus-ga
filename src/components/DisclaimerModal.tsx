83cb5baae41cd0fec9a6188b6c0687b9a377af37
DanielChee <Danielpchee@gmail.com>
2025-02-18 01:24:17 -0500

    disclaimer

--- src/components/DisclaimerModal.tsx
+++ src/components/DisclaimerModal.tsx
@@ -22,17 +22,20 @@ export default function Disclaimer(props: DisclaimerProps) {
       <div className="relative z-50 flex max-h-[80vh] max-w-3xl flex-col rounded-lg bg-white p-8 shadow-lg">
         <div className="mb-4 w-full">
           <div className="mb-4 flex justify-center">
-            <div className="text-2xl font-bold text-black">Disclaimer</div>
+            <div className="text-2xl font-bold text-black">Disclaimer:</div>
           </div>
-          <p className="text-base text-gray-500">
-            Lorem ipsum dolor sit amet consectetur. A in eu ullamcorper
-            consectetur. Diam sit dui leo turpis consectetur tempus. Erat
-            laoreet pellentesque tincidunt netus. Convallis enim tincidunt
-            gravida dui amet a lectus auctor aliquam. Senectus dui urna sit urna
-            lacus eu tortor scelerisque at. Donec adipiscing semper tincidunt
-            malesuada. Mauris aliquet etiam nisl eu sollicitudin scelerisque
-            nulla elementum. Eleifend ipsum dui eget vulputate justo ullamcorper
-            porttitor. Massa eros volutpat consectetur et ridiculus ac magna.
+          <p className="text-lg text-gray-600 text-center">
+          <p><strong>    Welcome to the FOCUS Community Page!</strong> <br />This forum is designed to allow FOCUS families to share ideas, resources, 
+                and questions with one another. Families are encouraged to make posts, 
+                comment, and share anything related to their current journey.
+
+
+                We kindly ask that you keep discussions civil and treat other FOCUS families 
+                with respect. Since the Community Page is an open forum, <u>FOCUS does not 
+                condone harmful or inappropriate content</u>. 
+                Any such posts can be reported, and the admin may take necessary action.
+                Thank you for your understanding.
+            </p>
           </p>
         </div>
         <div className="mt-4 flex justify-center">
@@ -41,9 +44,9 @@ export default function Disclaimer(props: DisclaimerProps) {
               document.body.style.overflow = "auto";
               onAccept();
             }}
-            className="text-bold inline-flex items-center justify-center gap-2.5 rounded-lg bg-theme-blue px-12 py-2 text-white transition hover:opacity-90"
+            className="text-bold inline-flex items-center justify-center gap-2.5 rounded-lg bg-theme-blue px-8 py-2 text-white transition hover:opacity-90"
           >
-            I agree
+            I understand
           </button>
         </div>
       </div>
