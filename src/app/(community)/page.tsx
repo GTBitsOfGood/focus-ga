c7b3c41c8d4621e65cb7b6b8646e2b1ae7d05d47
Lauren Ji <lyangji1011@gmail.com>
2025-03-07 13:04:35 -0500

    fix for clearing age

--- src/app/(community)/page.tsx
+++ src/app/(community)/page.tsx
@@ -8,9 +8,13 @@ import { LoaderCircle } from "lucide-react";
 import FilterComponent from "@/components/FilterComponent";
 import { Disability } from "@/utils/types/disability";
 import { Location } from "@/utils/types/location";
-import { Visiblity } from "@/utils/types/visibility"; 
+import { Visiblity } from "@/utils/types/visibility";
 import { AgeSelection, Filter } from "@/utils/types/common";
-import { PAGINATION_LIMIT } from "@/utils/consts";
+import {
+  MAX_FILTER_AGE,
+  MIN_FILTER_AGE,
+  PAGINATION_LIMIT,
+} from "@/utils/consts";
 import { useUser } from "@/contexts/UserContext";
 import { GEORGIA_CITIES } from "@/utils/cities";
 import { useSearch } from "@/contexts/SearchContext";
@@ -42,8 +46,12 @@ export default function Home() {
 
   const { searchTerm } = useSearch();
 
-  const [selectedVisibility, setSelectedVisibility] = useState<Visiblity[]>([{visibility: 'All', _id: 'All'}]);
-  const [selectedAge, setSelectedAge] = useState<AgeSelection[]>([{ minAge: 0, maxAge: 20 }]);
+  const [selectedVisibility, setSelectedVisibility] = useState<Visiblity[]>([
+    { visibility: "All", _id: "All" },
+  ]);
+  const [selectedAge, setSelectedAge] = useState<AgeSelection[]>([
+    { minAge: MIN_FILTER_AGE, maxAge: MAX_FILTER_AGE },
+  ]);
 
   const [totalPostsCount, setTotalPostsCount] = useState(0);
   const [pinnedPostContents, setPinnedPostContents] = useState<PopulatedPost[]>(
@@ -143,13 +151,19 @@ export default function Home() {
     selected: selectedAge,
     setSelected: (selected) => {
       handleVisibility(selected, setSelectedAge);
-    }
+    },
   };
 
   // fetch posts when filter changes
   useEffect(() => {
     fetchPosts(true);
-  }, [selectedDisabilities, selectedLocations, searchTerm, selectedVisibility, selectedAge])
+  }, [
+    selectedDisabilities,
+    selectedLocations,
+    searchTerm,
+    selectedVisibility,
+    selectedAge,
+  ]);
 
   // fetch posts when page changes
   const fetchPosts = async (clear: boolean = false) => {
@@ -170,12 +184,27 @@ export default function Home() {
         const newPage = clear ? 0 : page;
         const tags = selectedDisabilities.map((disability) => disability._id);
         const locations = selectedLocations.map((location) => location.name);
-        const visibility = user.isAdmin ? selectedVisibility[0].visibility : "All";
+        const visibility = user.isAdmin
+          ? selectedVisibility[0].visibility
+          : "All";
         const age = selectedAge[0];
 
-        const filters = { tags, locations, searchTerm, visibility, age, isFlagged: [false]};
+        const filters = {
+          tags,
+          locations,
+          searchTerm,
+          visibility,
+          age,
+          isFlagged: [false],
+        };
 
-        const {count, posts: newPosts } = await getPopulatedPosts(user._id, user.isAdmin, newPage * PAGINATION_LIMIT, PAGINATION_LIMIT, filters);
+        const { count, posts: newPosts } = await getPopulatedPosts(
+          user._id,
+          user.isAdmin,
+          newPage * PAGINATION_LIMIT,
+          PAGINATION_LIMIT,
+          filters,
+        );
         setTotalPostsCount(count);
         if (newPosts.length > 0) {
           setPosts(clear ? newPosts : [...posts, ...newPosts]);
--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -1,8 +1,9 @@
 "use client";
 
+import { MAX_FILTER_AGE, MIN_FILTER_AGE } from "@/utils/consts";
 import DropdownComponent from "./DropdownComponent";
 import RangeSliderComponent from "./RangeSliderComponent";
-import { Filter } from "@/utils/types/common"
+import { Filter } from "@/utils/types/common";
 
 type FilterProps = {
   filters: Filter<any>[];
@@ -11,7 +12,15 @@ type FilterProps = {
 export default function FilterComponent(props: FilterProps) {
   const handleClearFilters = () => {
     props.filters.map((filter) => {
-      filter.setSelected(null);
+      if (filter.label === "Age") {
+        filter.setSelected({
+          minAge: MIN_FILTER_AGE,
+          maxAge: MAX_FILTER_AGE,
+          _id: `age-${MIN_FILTER_AGE}-${MAX_FILTER_AGE}`,
+        });
+      } else {
+        filter.setSelected(null);
+      }
     });
   };
 
@@ -19,21 +28,31 @@ export default function FilterComponent(props: FilterProps) {
     <div className="flex flex-row justify-between">
       <div className="relative inline-block flex w-full items-center space-x-4">
         <label className="block text-sm font-medium">Filter By:</label>
-        {props.filters.map((filter, index) => (
-          filter.label === "Age" ? 
-          <RangeSliderComponent 
-            key={index}
-            label={filter.label}
-            minAge={0}
-            maxAge={20}
-            onChange={(minAge, maxAge) => {
-              filter.setSelected({ minAge, maxAge, _id: `age-${minAge}-${maxAge}` });
-            }}
-            initialMinAge={filter.selected.length > 0 ? filter.selected[0].minAge : 3}
-            initialMaxAge={filter.selected.length > 0 ? filter.selected[0].maxAge : 20}
-          /> :
-          <DropdownComponent key={index} filter={filter} />
-        ))}
+        {props.filters.map((filter, index) =>
+          filter.label === "Age" ? (
+            <RangeSliderComponent
+              key={index}
+              label={filter.label}
+              minAge={MIN_FILTER_AGE}
+              maxAge={MAX_FILTER_AGE}
+              onChange={(minAge, maxAge) => {
+                filter.setSelected({
+                  minAge,
+                  maxAge,
+                  _id: `age-${minAge}-${maxAge}`,
+                });
+              }}
+              initialMinAge={
+                filter.selected.length > 0 ? filter.selected[0].minAge : 3
+              }
+              initialMaxAge={
+                filter.selected.length > 0 ? filter.selected[0].maxAge : 20
+              }
+            />
+          ) : (
+            <DropdownComponent key={index} filter={filter} />
+          ),
+        )}
       </div>
       <button
         onClick={handleClearFilters}
