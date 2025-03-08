527afc82491666db8fdc00d480649120c3baa985
DanielChee <Danielpchee@gmail.com>
2025-03-08 12:50:20 -0500

    clear all button styling

--- src/components/FilterComponent.tsx
+++ src/components/FilterComponent.tsx
@@ -4,6 +4,7 @@ import { MAX_FILTER_AGE, MIN_FILTER_AGE } from "@/utils/consts";
 import DropdownComponent from "./DropdownComponent";
 import RangeSliderComponent from "./RangeSliderComponent";
 import { Filter } from "@/utils/types/common";
+import { useEffect, useState } from "react";
 
 type FilterProps = {
   setClearAll: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,12 +12,22 @@ type FilterProps = {
 };
 
 export default function FilterComponent(props: FilterProps) {
+  const [hasFilters, setHasFilters] = useState(false);
   const handleClearFilters = () => {
     props.setClearAll(true);
   };
 
+  useEffect(() => {
+    setHasFilters(
+      props.filters[0].selected.length > 0 ||
+      props.filters[1].selected.length > 0 ||
+      (props.filters[2].selected[0].minAge !== 0 || props.filters[2].selected[0].maxAge !== 20) ||
+      props.filters[3].selected[0].visibility !== 'All'
+    );
+  }, [props]);
+
   return (
-    <div className="relative inline-block flex w-full items-center space-x-4">
+    <div className="relative flex flex-wrap w-full items-center space-x-4">
       <label className="block text-sm font-medium">Filter By:</label>
       {props.filters.map((filter, index) =>
         filter.label === "Age" ? (
@@ -44,10 +55,13 @@ export default function FilterComponent(props: FilterProps) {
         ),
       )}
       <button
+        disabled={!hasFilters}
         onClick={handleClearFilters}
-        className="text-sm font-medium text-theme-gray"
+        className={`text-sm text-theme-blue font-bold ${
+          hasFilters ? "" : "hidden"
+        }`}
       >
-        Clear All
+        Clear All 
       </button>
     </div>
   );
