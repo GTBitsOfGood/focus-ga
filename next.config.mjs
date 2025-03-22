dacb0a448ca2d103434667aed7ed8927f3603d70
DanielChee <Danielpchee@gmail.com>
2025-03-22 15:15:24 -0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    experimental: {
+      serverActions: {
+        allowedOrigins: [
+          'https://focus-ga.my.site.com',
+          'https://focus-ga.netlify.app',
+        ],
+      },
+    },
+  };
 
 export default nextConfig;
